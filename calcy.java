610666359deccf9ad6ae066a53083717079a0b8b
Vaishnavi-Goud30 <lilly.goud30@gmail.com>
2026-08-19 18:04:59 +0530

    Create calcy.java

---
+++ calcy.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        // Create a scanner object to read user input
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("=== Simple Java Calculator ===");
+        
+        // Take the first number input
+        System.out.print("Enter first number: ");
+        double num1 = scanner.nextDouble();
+        
+        // Take the operator input
+        System.out.print("Enter an operator (+, -, *, /): ");
+        char operator = scanner.next().charAt(0);
+        
+        // Take the second number input
+        System.out.print("Enter second number: ");
+        double num2 = scanner.nextDouble();
+        
+        double result;
+        boolean validOperation = true;
+        
+        // Perform calculation based on the chosen operator
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                // Handle division by zero error
+                if (num2 == 0) {
+                    System.out.println("Error: Division by zero is not allowed.");
+                    validOperation = false;
+                    result = 0;
+                } else {
+                    result = num1 / num2;
+                }
+                break;
+            default:
+                System.out.println("Error: Invalid operator entered.");
+                validOperation = false;
+                result = 0;
+                break;
+        }
+        
+        // Print the result if the operation was successful
+        if (validOperation) {
+            System.out.println("Result: " + num1 + " " + operator + " " + num2 + " = " + result);
+        }
+        
+        // Close the scanner resource
+        scanner.close();
+    }
+}
